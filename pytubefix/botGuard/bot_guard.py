7c4a7b21d0cab29d25468867ecc3f66374041c4a
Atila Lima <atilalimade@gmail.com>
2025-04-03 13:26:31 -0300

    Update bot_guard.py: adding node js to PATH on runtime

--- pytubefix/botGuard/bot_guard.py
+++ pytubefix/botGuard/bot_guard.py
@@ -1,17 +1,16 @@
 import os
 import subprocess
-import sys
+import shutil
 
-PLATFORM = sys.platform
+os.environ["PATH"] += os.pathsep + "C:\\Program Files\\nodejs"
 
-NODE = 'node' if PLATFORM == 'linux' else 'node.exe'
-
-NODE_PATH = os.path.dirname(os.path.realpath(__file__)) + f'/binaries/{NODE}'
+NODE_PATH = os.path.abspath(shutil.which("node"))
+NODE_PATH = 'node' if NODE_PATH == None else NODE_PATH
 
 if not os.path.isfile(NODE_PATH):
     NODE_PATH = 'node'
 
-VM_PATH = os.path.dirname(os.path.realpath(__file__)) + '/vm/botGuard.js'
+VM_PATH = os.path.dirname(os.path.realpath(__file__)) + r'/vm/botGuard.js'
 
 def generate_po_token(visitor_data: str) -> str:
     """
@@ -23,4 +22,4 @@ def generate_po_token(visitor_data: str) -> str:
         [NODE_PATH, VM_PATH, visitor_data]
     ).decode()
     return result.replace("\n", "")
-
+    
